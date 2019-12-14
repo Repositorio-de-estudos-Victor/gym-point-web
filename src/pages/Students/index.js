a74aad90827eb1991d1f733a7aca078296b6a132
Victor Rodrigues Novais <victor96novais@gmail.com>
2019-12-14 16:40:41 -0300

    feature: header inside Students

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { MdAdd, MdSearch } from 'react-icons/md';
 
 import { Container } from './styles';
 
 export default function Students() {
-  return <Container />;
+  return (
+    <Container>
+      <strong>Gerenciador de Alunos</strong>
+      <div>
+        <button type="button">
+          <div>
+            <MdAdd size={16} color="#FFF" />
+          </div>
+          CADASTRAR
+        </button>
+        <div>
+          <span>
+            <MdSearch size={16} color="rgba(0, 0, 0, 0.2)" />
+          </span>
+          <input placeholder="Buscar aluno" />
+        </div>
+      </div>
+    </Container>
+  );
 }
--- src/pages/Students/styles.js
+++ src/pages/Students/styles.js
@@ -1,9 +1,65 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
-  max-width: 600px;
-  margin: 50px auto;
+  max-width: 1000px;
+  margin: 30px auto;
 
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  strong {
+    font-size: 20px;
+  }
+
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    button {
+      display: flex;
+      align-items: center;
+
+      margin: 0 15px 0;
+      height: 30px;
+      background: #ee4d64;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 12px;
+      transition: background 0.2s;
+      padding: 5px;
+
+      &:hover {
+        background: ${darken(0.04, '#ee4d64')};
+      }
+
+      div {
+        margin-right: 5px;
+      }
+    }
+
+    > div {
+      span {
+        position: absolute;
+        margin: 6px 6px 0;
+      }
+
+      input {
+        border: 1;
+        border-color: rgba(0, 0, 0, 0.1);
+        border-radius: 4px;
+        height: 30px;
+        padding: 0 25px;
+        color: #000;
+
+        &::placeholder {
+          color: rgba(0, 0, 0, 0.2);
+        }
+      }
+    }
+  }
 `;
