70e80a6c383b887343e26abd7dd464ddc1f3cb3d
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-02-05 12:25:34 -0300

    feature Box

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import { MdAdd, MdSearch } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Table } from './styles';
 
 export default function Students() {
   return (
     <Container>
-      <strong>Gerenciador de Alunos</strong>
-      <div>
-        <button type="button">
-          <div>
-            <MdAdd size={16} color="#FFF" />
-          </div>
-          CADASTRAR
-        </button>
+      <header>
+        <strong>Gerenciador de Alunos</strong>
         <div>
-          <span>
-            <MdSearch size={16} color="rgba(0, 0, 0, 0.2)" />
-          </span>
-          <input placeholder="Buscar aluno" />
+          <button type="button">
+            <div>
+              <MdAdd size={16} color="#FFF" />
+            </div>
+            CADASTRAR
+          </button>
+          <div>
+            <span>
+              <MdSearch size={16} color="rgba(0, 0, 0, 0.2)" />
+            </span>
+            <input placeholder="Buscar aluno" />
+          </div>
         </div>
-      </div>
+      </header>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>NOME</th>
+            <th>E-MAIL</th>
+            <th>IDADE</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Victor Novais</td>
+            <td>victor96novais@gmail.com</td>
+            <td>23</td>
+            <div>
+              <span>editar</span>
+              <span>apagar</span>
+            </div>
+          </tr>
+        </tbody>
+      </Table>
     </Container>
   );
 }
--- src/pages/Students/styles.js
+++ src/pages/Students/styles.js
@@ -3,62 +3,94 @@ import { darken } from 'polished';
 
 export const Container = styled.div`
   max-width: 1000px;
-  margin: 30px auto;
+  margin: 50px auto;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  strong {
-    font-size: 20px;
-  }
-
-  > div {
+  header {
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
+    align-self: center;
     align-items: center;
 
-    button {
-      display: flex;
-      align-items: center;
-
-      margin: 0 15px 0;
-      height: 30px;
-      background: #ee4d64;
-      font-weight: bold;
-      color: #fff;
-      border: 0;
-      border-radius: 4px;
-      font-size: 12px;
-      transition: background 0.2s;
-      padding: 5px;
-
-      &:hover {
-        background: ${darken(0.04, '#ee4d64')};
-      }
-
-      div {
-        margin-right: 5px;
-      }
+    strong {
+      font-size: 20px;
     }
 
     > div {
-      span {
-        position: absolute;
-        margin: 6px 6px 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      button {
+        display: flex;
+        align-items: center;
+
+        margin: 0 15px 0;
+        height: 30px;
+        background: #ee4d64;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+        border-radius: 4px;
+        font-size: 12px;
+        transition: background 0.2s;
+        padding: 5px;
+
+        &:hover {
+          background: ${darken(0.04, '#ee4d64')};
+        }
+
+        div {
+          margin-right: 5px;
+        }
       }
 
-      input {
-        border: 1;
-        border-color: rgba(0, 0, 0, 0.1);
-        border-radius: 4px;
-        height: 30px;
-        padding: 0 25px;
-        color: #000;
-
-        &::placeholder {
-          color: rgba(0, 0, 0, 0.2);
+      > div {
+        span {
+          position: absolute;
+          margin: 6px 6px 0;
         }
+
+        input {
+          border: 1;
+          border-color: rgba(0, 0, 0, 0.1);
+          border-radius: 4px;
+          height: 30px;
+          padding: 0 25px;
+          color: #000;
+
+          &::placeholder {
+            color: rgba(0, 0, 0, 0.2);
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Table = styled.table`
+  background-color: white;
+  margin: 20px auto;
+
+  thead {
+    font-size: 13px;
+    text-align: left;
+    padding: 8px;
+  }
+
+  tbody {
+    align-self: center;
+    align-items: center;
+
+    td {
+      padding: 0 10px 0;
+      text-align: left;
+    }
+
+    div {
+      padding: 0 10px 0;
+
+      span {
+        margin-right: 10px;
       }
     }
   }
